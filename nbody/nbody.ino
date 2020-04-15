842aac66546d00826aacf3a60eccfb8316618237
Jonny Dyer <jonny.dyer@gmail.com>
2020-04-15 07:14:08 -0700

    fixed nbody

--- nbody/nbody.ino
+++ nbody/nbody.ino
@@ -9,76 +9,147 @@
 #define B   A1
 #define C   A2
 
-#define FPS 300         // Maximum frames-per-second
-#define N_PARTICLES
+#define FPS 100         // Maximum frames-per-second
+#define N_PARTICLES 10
+#define ORIGIN_X 16
+#define ORIGIN_Y 8
+#define G 100.0f
 uint32_t prevTime = 0; // For frame-to-frame interval timing
 
-int hue[32][16];
+long hue[32][16];
 float value[32][16];
 
-RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, true);
 typedef struct particle {
-  float x,y,z,ux, uy, uz;
+  float x,y,z,ux, uy, uz, ax, ay, az;
   float m;
 }particle_t;
 
 particle_t g_particles[N_PARTICLES];
 
 void update(float dt);
+void seed_particle(particle_t &p);
+bool on_screen(int i, int j);
 
 void setup() {
   for(int i=0; i<32; i++){
     for(int j=0; j<16; j++){
-      hue[i][j] = 0.0f;
       value[i][j] = 0.0f;
+      hue[i][j] = 0;
     }
   }
 
   for(int i=0; i<N_PARTICLES; i++){
-    g_particles[i].x = random(0, 1e6)/1e5 - 5.0f;
-    g_particles[i].y = random(0, 1e6)/1e5 - 5.0f;
-    g_particles[i].z = random(0, 1e6)/1e5 - 5.0f;
-
-    g_particles[i].ux = random(0, 1e6)/1e6 - 0.5f;
-    g_particles[i].uy = random(0, 1e6)/1e5 - 0.5f;
-    g_particles[i].uz = random(0, 1e6)/1e5 - 0.5f;
+    seed_particle(g_particles[i]);
   }
   
   // put your setup code here, to run once:
   matrix.begin();
 }
 
-void update(float dt) {
-  int i, j;
+void seed_particle(particle_t &p){
+    p.x = random(0, 3e6)/1e5 - 15.0f;
+    p.y = random(0, 1.4e6)/1e5 - 7.0f;
+    p.z = random(0, 1e6)/1e5 - 5.0f;
 
-  float fx, fy, fz;
+    p.ux = random(0, 1e6)/1e4 - 50.0f;
+    p.uy = random(0, 1e6)/1e4 - 50.0f;
+    p.uz = random(0, 1e6)/1e4 - 50.0f;
 
-  for(n=0; n<N_PARTICLES;n++){
-    fx = 0;
-    fy = 0;
-    fz = 0;
+    p.ax = 0.0f;
+    p.ay = 0.0f;
+    p.az = 0.0f;
+
+    p.m = random(1, 1e4)/10;
+}
+
+bool on_screen(int i, int j)
+{
+  if((i < 0) || (i > 31)){
+    return false;
+  }else if((j < 0) || (j > 15)){
+    return false;
   }
+  return true;
+}
 
-  hue[i][j] = (int)((y1 + 30.0f) * 7.0f + 5.0f*t);
-  value[i][j] = 255;
+void update(float dt) {
+  int i, j, k;
+  float x, y, z, r2, rx, ry, rz, m1_i, m2, rmag;
+  particle_t *p;
+  
+  for(i=0; i<N_PARTICLES;i++){
+    p = &g_particles[i];
+    p->ax = 0.0f;
+    p->ay = 0.0f;
+    p->az = 0.0f;
+    x = p->x;
+    y = p->y;
+    z = p->z;
+    m1_i = 1.0f / p->m;
+
+    // TODO: Big optimization possible here -
+    // Instead of N^2 iteration, we can compute pair-wise
+    // force components and then compute accelerations based on sum
+    // of forces. This will result in n*(n-1)/2 evals instead
+    // of n^2
+    for(j=0; j<N_PARTICLES; j++){
+      if(i == j){
+        continue;
+      }
+      rx = (g_particles[j].x - x);
+      ry = (g_particles[j].y - y);
+      rz = (g_particles[j].z - z);
+      m2 = g_particles[j].m;
+      r2 = rx * rx + ry * ry + rz * rz;
+      //rmag = sqrt(r2);
+      
+      // G * m1 * m2 * rx / |r| / r^2
+      p->ax += G * m2 * rx / (r2 + 1);
+      p->ay += G * m2 * ry / (r2 + 1);
+      p->az += G * m2 * rz / (r2 + 1);
+    }
+    p->ax *= m1_i;
+    p->ay *= m1_i;
+    p->az *= m1_i;
+    p->ux += p->ax * dt;
+    p->uy += p->ay * dt;
+    p->uz += p->az * dt;
+
+    p->x += p->ux * dt;
+    p->y += p->uy * dt;
+    p->z += p->uz * dt;
+  }
 
   for(i=0; i<32; i++){
     for(j=0; j<16; j++){
-      value[i][j] *= 0.9995;
-      matrix.drawPixel(i, j, matrix.ColorHSV(hue[i][j], 255, (int)value[i][j], true));
+      value[i][j] *= 0.99;
+      matrix.drawPixel(i, j, matrix.ColorHSV(hue[i][j], 100, (int)value[i][j], true));
+      //matrix.drawPixel(i, j, matrix.ColorHSV(hue[i][j], 255, 0, true));
     }
   }
   
-  //matrix.drawLine((uint8_t)(0.9*x + 15.0), (uint8_t)(z * 0.33f), 
-  //               (uint8_t)(0.9*x1 + 15.0), (uint8_t)(z1 * 0.33f), 
-  //                matrix.ColorHSV((int)((y1 + 30.0f) * t), 255, 255, true));
-  x = x1;
-  y = y1;
-  z = z1;
+  for(k=0; k<N_PARTICLES; k++){
+    p = &g_particles[k];
+    i = (int)p->x + ORIGIN_X;
+    j = (int)p->y + ORIGIN_Y;
+    if(on_screen(i, j)){
+      value[i][j] = 255;
+      m2 = p->uz;
+      if(m2 > 0){
+        hue[i][j] = 0;
+        matrix.drawPixel(i, j, matrix.ColorHSV(0, 0.01*m2*m2, 255, true));
+      }else {
+        hue[i][j] = 800;
+        matrix.drawPixel(i, j, matrix.ColorHSV(800, 0.01*m2*m2, 255, true));
+      }
+    } 
+  }
+  
 }
 
 void loop() {
-  update(0.005);
+  update(0.001);
   
   uint32_t t;
   while(((t = millis()) - prevTime) < (1000 / FPS));
